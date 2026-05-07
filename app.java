1e0ee15caf4cddde46d56a93a2de2468f15e663d
jithu <jithunikhila143@gmail.com>
2026-05-07 13:55:53 +0000

    GHA

---
+++ app.java
@@ -0,0 +1,29 @@
+public class App {
+
+	    public static void main(String[] args) {
+
+		            System.out.println("=================================");
+			            System.out.println("   Welcome to Java GHA Demo");
+				            System.out.println("=================================");
+
+					            String appName = "Bank Application";
+						            String environment = "Development";
+							            int port = 8080;
+
+								            System.out.println("Application Name : " + appName);
+									            System.out.println("Environment      : " + environment);
+										            System.out.println("Application Port : " + port);
+
+											            System.out.println("---------------------------------");
+
+												            int users = 150;
+													            double balance = 45890.75;
+
+														            System.out.println("Active Users     : " + users);
+															            System.out.println("Bank Balance     : ₹" + balance);
+
+																            System.out.println("---------------------------------");
+
+																	            System.out.println("Java CI/CD Pipeline Success 🚀");
+																		        }
+}
